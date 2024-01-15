52e5f467e274bd5182cf668c25e146250d8bacbc
Daniel Rosselló <daniel.rossello@secuoyas.com>
2024-01-15 13:47:05 +0100

    FIX resolve_url_basename_time_file_size

--- lib/custom_download_strategy.rb
+++ lib/custom_download_strategy.rb
@@ -108,7 +108,7 @@ class GitHubPrivateRepositoryReleaseDownloadStrategy < GitHubPrivateRepositoryDo
     _, @owner, @repo, @tag, @filename = *@url.match(url_pattern)
   end
 
-  def resolve_url_basename_time_file_size(url: , timeout: nil)
+  def resolve_url_basename_time_file_size(url, timeout: nil)
       [download_url, "", Time.now, 0, false]
     end
 
@@ -120,7 +120,7 @@ class GitHubPrivateRepositoryReleaseDownloadStrategy < GitHubPrivateRepositoryDo
 
   def _fetch(url:, resolved_url:, timeout:)
 
-  resolve_url_basename_time_file_size(url: @url)
+    resolve_url_basename_time_file_size(@url)
 
     # HTTP request header `Accept: application/octet-stream` is required.
     # Without this, the GitHub API will respond with metadata, not binary.
