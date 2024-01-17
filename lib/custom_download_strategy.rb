c280ba7da11f5d40c319fb93f0d205ea855f21ea
danielrosse <98873656+danielrosse@users.noreply.github.com>
2024-01-17 12:50:38 +0100

    Update custom_download_strategy.rb
    FIX function to download metadata

--- lib/custom_download_strategy.rb
+++ lib/custom_download_strategy.rb
@@ -133,7 +133,8 @@ class GitHubPrivateRepositoryReleaseDownloadStrategy < GitHubPrivateRepositoryDo
   end
 
   def fetch_release_metadata
-    release_url = "https://api.github.com/repos/#{@owner}/#{@repo}/releases/tags/#{@tag}"
-    GitHub.open_api(release_url)
+    #release_url = "https://api.github.com/repos/#{@owner}/#{@repo}/releases/tags/#{@tag}"
+    #GitHub::API.open_rest(release_url)
+    GitHub.get_release(@owner, @repo, @tag)
   end
 end
